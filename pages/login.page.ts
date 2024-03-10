724c16043cc66fd2b71fc73fc7d05d57a4236043
Anastasiia <zak.anastasiia@gmail.com>
2024-03-10 18:51:00 +0200

    add page object add new test

---
+++ pages/login.page.ts
@@ -0,0 +1,28 @@
+import { Page } from '@playwright/test';
+export class Login {
+    page: Page;
+constructor(page: Page) {
+    this.page = page;
+}
+
+     async  open(){
+        await this.page.goto('https://shopdemo-alex-hot.koyeb.app/login');
+      }
+     async  login(email: string, password: string){
+        await this.page
+            .getByRole('main')
+            .getByPlaceholder('Please Enter Your Email')
+            .fill(email);
+        await this.page.getByPlaceholder('Please Enter Your Password').fill(password);
+        await this.page.getByRole('button', { name: 'Login' }).click();
+      }
+    
+    
+     async openAndLogin(email: string, password: string) {
+        await this.open();
+        await this.login(email, password);
+      }
+
+}
+
+
--- tests/purchase.test.ts
+++ tests/purchase.test.ts
@@ -1,16 +1,13 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
+import { Login } from '../pages/login.page';
+
+const email = 'test9@test.com'
+const password = 'test9@test.com'
 
 test('can purchase product', async ({ page }) => {
-  const email = 'test9@test.com'
-  const password = 'test9@test.com'
-
   //login
-  await page.goto('https://shopdemo-alex-hot.koyeb.app/');
-  await page.getByRole('link', { name: 'Welcome! ' }).click();
-  await page.getByRole('menuitem', { name: 'Login' }).click();
-  await page.getByRole('main').getByPlaceholder('Please Enter Your Email').fill('test9@test.com');
-  await page.getByPlaceholder('Please Enter Your Password').fill('test9@test.com');
-  await page.getByRole('button', { name: 'Login' }).click();
+  const login = new Login(page);
+await login.openAndLogin(email, password);
 
 
   //purchase
@@ -22,3 +19,25 @@ test('can purchase product', async ({ page }) => {
   await page.getByRole('button', { name: 'Place Order' }).click();
   await expect(page.getByRole('heading', {level: 2, name: 'Thank you for your order.'})).toBeVisible();
 });
+
+test('can purchase 2 products', async ({ page }) => {
+
+  //login
+  const login = new Login(page);
+await login.openAndLogin(email, password);
+  //purchase
+  await page.getByRole('link', { name: 'Brands ' }).click();
+  await page.locator('div').filter({ hasText: /^Nizhyn cannery$/ }).first().click();
+  await page.getByRole('menuitem', { name: 'Nizhyn cannery' }).click();
+  await page.getByRole('link', { name: 'CHERRY TOMATOES By Nizhyn' }).click();
+  await page.getByRole('button', { name: 'Add To Bag' }).click();
+
+  await page.goto('https://shopdemo-alex-hot.koyeb.app/');
+  await page.getByRole('link', { name: 'Brands ' }).click();
+  await page.locator('div').filter({ hasText: /^Nizhyn cannery$/ }).nth(1).click();
+  await page.getByRole('menuitem', { name: 'Nizhyn cannery' }).click();
+  await page.getByRole('link', { name: 'MARINATED CUCUMBERS NEZHIN' }).click();
+  await page.getByRole('button', { name: 'Add To Bag' }).click();
+  await page.getByRole('button', { name: 'Place Order' }).click();
+  await expect(page.getByRole('heading', {level: 2, name: 'Thank you for your order.'})).toBeVisible();
+  });
